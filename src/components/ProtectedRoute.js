7d57b21ad5a9147b3bfba44efa2301b97bd40f13
Tony Nguyen <tng2001811@gmail.com>
2021-08-11 23:44:31 -0400

    fixed issue on protected route

--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import {Route, Redirect} from 'react-router-dom';
 
-const ProtectedRoute = ({component: Component, authedUser, ...rest}) => (
-    <Route
+const ProtectedRoute = ({ component: Comp, authedUser, path, ...rest }) => {
+    return (
+      <Route
+        path={path}
         {...rest}
-        render={props => (
-            authedUser
-                ? <Component {...props} />
-                : <Redirect to={{
-                    pathname: '/',
-                    state: {from: props.location.pathname}
-                }}/>
-        )}
-    />
-);
-
+        render={(props) => {
+          return authedUser 
+            ? <Comp {...props} /> 
+            : <Redirect to={{
+                pathname: '/',
+                state: {from: props.location.pathname}
+            }}/>;
+        }}
+      />
+    );
+  };
 export default ProtectedRoute;
